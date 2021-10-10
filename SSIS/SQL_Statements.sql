0a165c66fdffbea4591249faea1736dda36cebeb
nghoanglong <19521788@gm.uit.edu.vn>
2021-10-10 23:32:14 +0700

    complete SSIS - part 2

--- SSIS/SQL_Statements.sql
+++ SSIS/SQL_Statements.sql
@@ -2,8 +2,8 @@ CREATE DATABASE Datalake_MarketingAnalytics;
 CREATE DATABASE DataWarehouse_MarketingAnalytics;
 
 USE DataWarehouse_MarketingAnalytics;
+USE Datalake_MarketingAnalytics;
 
 SELECT * FROM dbo.Fact_MarketingAnalytic;
 SELECT * FROM dbo.Dim_Customer;
 SELECT * FROM dbo.Dim_Date;
-
